7501ac48bc568f47b073bde05c71594a0d737c47
Mithrall <kenneth-dorn@hotmail.com>
2016-09-13 15:53:29 +0200

    Everything but QU exeption working

--- 04_Pig_Latin/Translator.cs
+++ 04_Pig_Latin/Translator.cs
@@ -1,7 +1,27 @@
 ﻿namespace _04_Pig_Latin {
     internal class Translator {
         public string Translate(string s) {
-            return s;
+            string result = "";
+            string IsVowel = "aeioy"; //mangler QU untagelsen så derfor ikke "u" her enddu
+
+            //for each word in string
+            foreach (string word in s.Split(' ')) {
+                //temporary word
+                string tempWord = word;
+                
+                //while word doesn't start on vowel
+                while (IsVowel.IndexOf(tempWord.Substring(0, 1)) < 0) { 
+                    tempWord += tempWord[0]; //add first letter to the end
+                    tempWord = tempWord.Substring(1); //remove first letter
+                }
+                //add "ay" når der ik er flere konsonanter i starten af ordet
+                tempWord += "ay";
+                //add word and a space to final result 
+                result += tempWord + ' ';
+            }
+
+            //return + remove exess spaces
+            return result.TrimEnd();
         }
     }
 }
